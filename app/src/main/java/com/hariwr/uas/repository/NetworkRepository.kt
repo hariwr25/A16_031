71b92b4723d0f19e1a2537f9efc3a212c4f3ca03
hariwr <hariwr25@gmail.com>
2025-01-29 07:27:43 +0700

    Menambahkan Fungsi untuk menangani respons jaringan secara umum

--- app/src/main/java/com/hariwr/uas/repository/NetworkRepository.kt
+++ app/src/main/java/com/hariwr/uas/repository/NetworkRepository.kt
@@ -3,5 +3,17 @@ package com.hariwr.uas.repository
 import com.hariwr.uas.network.ApiService
 
 class NetworkRepository(private val apiService: ApiService) {
-
+    // Fungsi untuk menangani respons jaringan secara umum
+    private suspend fun <T> makeRequest(call: suspend () -> retrofit2.Response<T>): Result<T> {
+        return try {
+            val response = call()
+            if (response.isSuccessful && response.body() != null) {
+                Result.success(response.body()!!)
+            } else {
+                Result.failure(Throwable("Failed to load data: ${response.message()}"))
+            }
+        } catch (e: Exception) {
+            Result.failure(e) // Mengembalikan error jika terjadi exception
+        }
+    }
 }
