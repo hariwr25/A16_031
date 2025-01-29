c82e6c02785fdbe27ac70032f4e93fcba484025f
hariwr <hariwr25@gmail.com>
2025-01-29 07:59:48 +0700

    Menambahkan function homescreen

---
+++ app/src/main/java/com/hariwr/uas/ui/view/HomeScreen.kt
@@ -0,0 +1,11 @@
+package com.hariwr.uas.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.hariwr.uas.ui.viewmodel.HomeViewModel
+
+@Composable
+fun HomeScreen(viewModel: HomeViewModel = viewModel(), navController: NavController) {
+
+}
