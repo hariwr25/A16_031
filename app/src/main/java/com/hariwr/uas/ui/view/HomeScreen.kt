d581336c6edca2996f4391b112b8b92666b5cdc2
hariwr <hariwr25@gmail.com>
2025-01-29 08:00:32 +0700

    Mengambil data dari LiveData

--- app/src/main/java/com/hariwr/uas/ui/view/HomeScreen.kt
+++ app/src/main/java/com/hariwr/uas/ui/view/HomeScreen.kt
@@ -1,11 +1,16 @@
 package com.hariwr.uas.ui.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.hariwr.uas.ui.viewmodel.HomeViewModel
 
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = viewModel(), navController: NavController) {
-
+// Mengambil data dari LiveData
+    val saldo by viewModel.saldo.observeAsState(0.0)
+    val totalPendapatan by viewModel.totalPendapatan.observeAsState(0.0)
+    val totalPengeluaran by viewModel.totalPengeluaran.observeAsState(0.0)
 }
